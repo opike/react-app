91b9f047be10d660f1b723fb3d75f3dc666840ef
Oliver Pike <oliver@hipocampo.com>
2021-01-22 09:08:56 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+// Domain: http://parsimonious-religion.surge.sh
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 2
         </a>
       </header>
     </div>
